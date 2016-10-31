3b7bba4d518c0820707389df8465c2f022265edc
katerabinowitz <cathryn.rabinowitz@gmail.com>
2016-10-31 19:31:01 -0400

    create single+multi sums

--- meetupDataSum.py
+++ meetupDataSum.py
@@ -4,14 +4,18 @@ import numpy as np
 meetup = pd.read_csv('techMeetupsNotated.csv')
 print meetup.info()
 
-
 meetup=meetup[meetup['applicable'] == 1]
 
 meetup=meetup.drop(['applicable','desc','name','time'],axis=1)
 
 meetup['singleSpeaker'] = np.where(meetup['male'] + meetup['female'] == 1, 'yes', 'no')
 
-meetupSum=meetup.groupby(['group','singleSpeaker']).sum().add_prefix('sum_')
-print meetupSum
+meetupT=pd.melt(meetup, id_vars=['group','singleSpeaker'], value_vars=['male','female','nonBinary'])
 
-meetupSum.to_csv('meetupGenderSum.csv')
+meetupSum=meetupT.groupby(['group','singleSpeaker','variable']).sum().add_prefix('sum_')
+
+meetupSingle= meetupSum.loc[(meetupSum.index.get_level_values('singleSpeaker') == 'yes')]
+meetupMulti= meetupSum.loc[(meetupSum.index.get_level_values('singleSpeaker') == 'no')]
+
+meetupSingle.to_csv('meetupSingleSum.csv')
+meetupMulti.to_csv('meetupSingleMulti.csv')
