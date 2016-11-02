1385fd659ecf9d3582cd980847682c6f937f661d
katerabinowitz <cathryn.rabinowitz@gmail.com>
2016-11-02 17:39:38 -0400

    cleanup

--- meetupSpeakerWide.py
+++ meetupSpeakerWide.py
@@ -12,7 +12,10 @@ meetup['singleSpeaker'] = np.where(meetup['male'] + meetup['female'] == 1, 'yes'
 meetupSum=meetup.groupby(['group','singleSpeaker']).sum().add_prefix('sum_')
 
 meetupSingle= meetupSum.loc[(meetupSum.index.get_level_values('singleSpeaker') == 'yes')]
+meetupSingle.index = meetupSingle.index.droplevel(1)
+
 meetupMulti= meetupSum.loc[(meetupSum.index.get_level_values('singleSpeaker') == 'no')]
+meetupMulti.index = meetupMulti.index.droplevel(1)
 
 meetupSingle.to_csv('meetupSingleSum.csv')
 meetupMulti.to_csv('meetupSingleMulti.csv')
